9a9b98c7beaac0514b465b85d194a1b8794d499c
PranjaliJain <pranjali.jain007@gmail.com>
2022-06-01 14:38:59 -0700

    code cleanup

--- miter.py
+++ miter.py
@@ -3,23 +3,16 @@ from pysmt.shortcuts import Symbol, And, Not, Or, is_sat, Xor
 import dimacs
 
 vars_cnt, clauses, comments = dimacs.read_dimacs('cnf_bench/Multi-AND/mand4.cnf')
+res1, rev_st1 = dimacs.dimacs_to_pysmt(vars_cnt, clauses, comments)
 
-res, rev_st = dimacs.dimacs_to_pysmt(vars_cnt, clauses, comments)
+vars_cnt, clauses, comments = dimacs.read_dimacs('cnf_bench/Multi-AND/mand3.cnf')
+res2, rev_st2 = dimacs.dimacs_to_pysmt(vars_cnt, clauses, comments)
 
 print(vars_cnt, clauses, comments)
 print("number of clauses: ", len(clauses))
-print(res, rev_st)
-# varA = Symbol("A")
-f = Xor(res, res)
+print(res1, rev_st1)
+f = Xor(res1, res2)
 
 sat_res = is_sat(f)
-# assert sat_res # SAT
-print("dimacs := %s is SAT? %s" % (res, sat_res))
-print("if UNSAT, given circuits are equal")
-# assert not sat_res # UNSAT
-lst = [f,f,f]
-miter = Or(lst)
-print("miter", miter)
-
-sat_res = is_sat(miter)
-print("is SAT miter ", sat_res)
+print("dimacs := %s is SAT? %s" % (f, sat_res))
+print("If UNSAT, given circuits are equal")
--- pyeda_test.py
+++ pyeda_test.py
@@ -1,12 +1,16 @@
 import pyeda
 from pyeda.boolalg.expr import exprvar, Or, And, expr2dimacscnf
+
+## Circuit - 1 
 a, b, c = map(exprvar, 'abc')
 f1 = Or(And(~a, ~b, ~c), And(~a, ~b, c), And(a, ~b, c), And(a, b, c), And(a, b, ~c))
 ex = f1.to_cnf()
 exp, dimacs = expr2dimacscnf(ex)
 print(dimacs)
+
+
+## Circuit - 2
 f2 = Or(And(~a, ~b, c), And(a, ~b, c), And(a, b, c), And(a, b, ~c))
 ex = f2.to_cnf()
 exp, dimacs = expr2dimacscnf(ex)
 print(dimacs)
-# print(ex)
